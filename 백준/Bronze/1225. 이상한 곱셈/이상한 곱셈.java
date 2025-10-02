be6bb94a0d33d7ce6e2fae4ce24e7c19658b5672
ssseft <147024411+ssseft@users.noreply.github.com>
2025-10-03 08:14:21 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 276 ms, Memory: 18660 KB -BaekjoonHub

---
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		
+		long ans = 0;
+		
+		for(int i = 0;i<a.length();i++) {
+			for(int j = 0;j<b.length();j++) {
+				ans += (a.charAt(i)-'0')*(b.charAt(j)-'0');
+			}
+		}
+		System.out.print(ans);
+	}
+}
