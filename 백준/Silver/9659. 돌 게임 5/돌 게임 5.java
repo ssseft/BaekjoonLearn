e3e68968f1d3b0c2f67af0a26188fe650e567347
ssseft <147024411+ssseft@users.noreply.github.com>
2025-11-03 13:45:38 +0900

    [Silver III] Title: 돌 게임 5, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long N = sc.nextLong();
+		if(N%2 == 1) System.out.print("SK");
+		else System.out.print("CY");
+	}
+}
