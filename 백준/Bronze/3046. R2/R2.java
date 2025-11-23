deb1493b7b52fda8d1dd137a8dce69794b964b8a
ssseft <147024411+ssseft@users.noreply.github.com>
2025-11-23 22:57:30 +0900

    [Bronze IV] Title: R2, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int R1=Integer.parseInt(st.nextToken());
+        int S=Integer.parseInt(st.nextToken());
+        int R2 = 2*S - R1;
+        System.out.print(R2);
+    }
+}
