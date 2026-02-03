e0c88eaa30df95ee3cc54065a63123b0aaaad1b8
ssseft <147024411+ssseft@users.noreply.github.com>
2026-02-04 08:35:54 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 148 ms, Memory: 17504 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main{
+	public static void main(String[] a){
+		Scanner sc=new Scanner(System.in);
+		if(sc.next().equals(sc.next()))System.out.print(0);
+		else System.out.print(1550);
+	}
+}
