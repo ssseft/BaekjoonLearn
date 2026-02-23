fb27f8adc4112feeb240a6b85343ffaf0164721b
ssseft <147024411+ssseft@users.noreply.github.com>
2026-02-23 15:26:08 +0900

    [Gold II] Title: 모양 정돈, Time: 288 ms, Memory: 27232 KB -BaekjoonHub

--- 백준/Gold/2450. 모양 정돈/모양 정돈.java
+++ 백준/Gold/2450. 모양 정돈/모양 정돈.java
@@ -1,62 +1,90 @@
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-	static int n;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		int[] arr = new int[n];
-		sc.nextLine();
-		StringTokenizer st = new StringTokenizer(sc.nextLine());
-		int[] cnt = new int[4];
-		for(int i = 0;i<n;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			cnt[arr[i]]++;
-		}
-		
-		int ans = Integer.MAX_VALUE;
-		for(int i = 1;i<=3;i++) {
-			for(int j = 1;j<=3;j++) {
-				for(int k = 1;k<=3;k++) {
-					if(i==j||j==k||i==k) continue;
-					ans = Math.min(ans, cal(i,j,k,cnt,arr));
-				}
-			}
-		}
-		
-		System.out.print(ans);
-	}
-	static int cal(int a,int b,int c,int[] cnt,int[] arr) {
-		int t = 0;
-		int[][] sta = new int[4][4];
-		for(int i = 0;i<cnt[a];i++) {
-			if(arr[i]==a) continue;
-			sta[a][arr[i]]++;
-		}
-		for(int i = cnt[a];i<cnt[a]+cnt[b];i++) {
-			if(arr[i]==b) continue;
-			sta[b][arr[i]]++;
-		}
-		for(int i = cnt[a]+cnt[b];i<n;i++) {
-			if(arr[i]==c) continue;
-			sta[c][arr[i]]++;
-		}
-		
-		for(int i = 1;i<=3;i++) {
-			for(int j = 1;j<=3;j++) {
-				if(i==j) continue;
-				if(sta[i][j]>0&&sta[j][i]>0) {
-					int min = Math.min(sta[i][j], sta[j][i]);
-					t += min;
-					sta[i][j] -= min;
-					sta[j][i] -= min;
-				}
-			}
-		}
-		
-		for(int i = 1;i<=3;i++) t += sta[i][1]*2;
-		
-		return t;
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); // 3 <= N <= 100,000
+        int[] arr = new int[N];
+        int[] count = new int[3]; // 1, 2, 3의 개수를 세기 위한 배열
+
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+            count[arr[i] - 1]++;
+        }
+
+        int minCount = Integer.MAX_VALUE;
+
+        // 6가지 경우의 수를 계산하여 최소 개수를 찾음
+        // 1, 2, 3 순서
+        int[] order = getTargetOrder(count, 1, 2, 3);
+        minCount = Math.min(minCount, getMinSwipingTime(arr, order));
+
+        // 1, 3, 2 순서
+        order = getTargetOrder(count, 1, 3, 2);
+        minCount = Math.min(minCount, getMinSwipingTime(arr, order));
+
+        // 2, 1, 3 순서
+        order = getTargetOrder(count, 2, 1, 3);
+        minCount = Math.min(minCount, getMinSwipingTime(arr, order));
+
+        // 2, 3, 1 순서
+        order = getTargetOrder(count, 2, 3, 1);
+        minCount = Math.min(minCount, getMinSwipingTime(arr, order));
+
+        // 3, 1, 2 순서
+        order = getTargetOrder(count, 3, 1, 2);
+        minCount = Math.min(minCount, getMinSwipingTime(arr, order));
+
+        // 3, 2, 1 순서
+        order = getTargetOrder(count, 3, 2, 1);
+        minCount = Math.min(minCount, getMinSwipingTime(arr, order));
+
+        System.out.println(minCount);
+    }
+
+    private static int[] getTargetOrder(int[] count, int first, int second, int third) {
+        int[] order = new int[count[0] + count[1] + count[2]];
+
+        for (int i = 0; i < count[first - 1]; i++) order[i] = first;
+        for (int i = count[first - 1]; i < count[first - 1] + count[second - 1]; i++) order[i] = second;
+        for (int i = count[first - 1] + count[second - 1]; i < order.length; i++) order[i] = third;
+
+        return order;
+    }
+
+    private static int getMinSwipingTime(int[] arr, int[] targets) {
+        int[][] change = new int[4][4]; // change[i][j] = i에서 j로 바꿔야 하는 개수
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != targets[i]) {
+                change[arr[i]][targets[i]]++;
+            }
+        }
+
+        int count = 0;
+        int min;
+        // 1 <-> 2, 1 <-> 3, 2 <-> 3 간의 직접 스와이프를 최대한 활용
+        min = Math.min(change[1][2], change[2][1]);
+        count += min; // 1 <-> 2
+        
+        change[1][2] -= min;
+        change[2][1] -= min;
+        
+        min = Math.min(change[1][3], change[3][1]);
+        count += Math.min(change[1][3], change[3][1]); // 1 <-> 3
+        change[1][3] -= min;
+        change[3][1] -= min;
+        
+        min = Math.min(change[2][3], change[3][2]);
+        count += min;
+        change[2][3] -= min;
+        change[3][2] -= min;
+
+        // 남은 변경은 2회 스와이프가 필요하므로 2를 곱함
+        count += (change[1][2] + change[1][3]) * 2;
+
+        return count;
+    }
 }
