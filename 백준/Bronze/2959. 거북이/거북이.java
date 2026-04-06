db9ebba9666bbfba395144c09c9e981285507dad
ssseft <147024411+ssseft@users.noreply.github.com>
2026-04-06 22:42:18 +0900

    [Bronze III] Title: 거북이, Time: 172 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Bronze/2959. 거북이/거북이.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[4];
+		for(int i=  0;i<4;i++) arr[i] = sc.nextInt();
+		Arrays.sort(arr);
+		System.out.print(arr[0]*arr[2]);
+	}
+}
